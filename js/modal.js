c1f5d9298677a93737add8afbf3369e70ba6371c
Denis Onyshchuk <o.82.denis@gmail.com>
2020-06-30 22:33:20 +0300

    doing my homework

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    blureWrapper: document.querySelector("[data-blur-wrapper"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+
+    refs.modal.classList.toggle("is-hidden");
+
+    refs.blureWrapper.classList.toggle("blur");
+  }
+})();
